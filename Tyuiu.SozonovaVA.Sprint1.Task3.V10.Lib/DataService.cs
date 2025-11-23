c7a1532624e1a1ef0284338d7e88df8b32fe5d8c
Варвара Созонова <Tyuiu.SozonovaVA@outlook.com>
2025-11-23 20:48:49 +0500

    Task3

---
+++ Tyuiu.SozonovaVA.Sprint1.Task3.V10.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.SozonovaVA.Sprint1.Task3.V10.Lib
+{
+    public class DataService : ISprint1Task3V10
+    {
+        public string NumberToMoney(double number)
+        {
+            int rubles = (int)number;
+            int kopecks = (int)((number - rubles) * 100);
+
+            string formattedNumber = number.ToString().Replace(',', '.');
+
+            return $"{formattedNumber} руб. - это {rubles} руб. {kopecks} коп.".Trim();
+        }
+    }
+}
---
+++ Tyuiu.SozonovaVA.Sprint1.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.SozonovaVA.Sprint1.Task3.V10.Lib;
+namespace Tyuiu.SozonovaVA.Sprint1.Task3.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double number = 23.6;
+            Assert.AreEqual(23.6, number);
+        }
+    }
+}
