a430727794f4211aacbd0ff7d1604be3728e8392
Варвара Созонова <Tyuiu.SozonovaVA@outlook.com>
2025-11-23 21:27:33 +0500

    Task4

---
+++ Tyuiu.SozonovaVA.Sprint1.Task4.V27.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.SozonovaVA.Sprint1.Task4.V27.Lib;
+namespace Tyuiu.SozonovaVA.Sprint1.Task4.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalc()
+        {
+            var result = new DataService();
+            Assert.AreEqual(-2.414, result.Calculate(1, 2));
+        }
+    }
+}
